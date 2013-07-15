670e81abd1eb1600dc6fcf3cdfad0596c889704b
agatov <agatovs@gmail.com>
2013-07-15 15:58:32 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Landing::Application.config.secret_token = 'be701ad5b828e8c8049783c65fb0c6c5ba9d973a59484bc288699f173b157519831f7042a06b58cd90814ffc2a061b2ec4e8c459a808c049401dddd4b1eb5e74'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Landing::Application.routes.draw do
+  root to: 'application#index'
+end
